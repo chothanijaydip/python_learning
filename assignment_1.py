81fcaa9e7724cc45e5f7e0b104f170f5ecb614ed
Jaydip Chothani <32288904+chothanijaydip@users.noreply.github.com>
2025-02-25 12:22:09 +0530

    Create assignment_1.py

---
+++ assignment_1.py
@@ -0,0 +1,21 @@
+
+'''
+Task 1
+'''
+
+a = input("Enter the first Number :")
+b = input("Enter the second Number :")
+
+print("Addition:",int(a)+int(b))
+print("subtraction:",int(a)-int(b))
+print("Multiplication:",int(a)*int(b))
+print("Division:",int(a)/int(b))
+
+
+'''
+Task 2
+'''
+
+first = input("Enter the first name : ")
+last = input("Enter the last name : ")
+print("Hello,",first,last+"!"," Welcome to the Python program.")
