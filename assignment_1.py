0c5ba7eae9d1d5ec9a7517a37527139411d0f3e6
Jaydip Chothani <32288904+chothanijaydip@users.noreply.github.com>
2025-02-25 12:27:11 +0530

    Update assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -6,11 +6,13 @@ Task 1
 a = input("Enter the first Number :")
 b = input("Enter the second Number :")
 
-print("Addition:",int(a)+int(b))
-print("subtraction:",int(a)-int(b))
-print("Multiplication:",int(a)*int(b))
-print("Division:",int(a)/int(b))
-
+if a.strip() == '' or b.strip() == '':
+    print("Error: One or both inputs are empty!")
+else:
+    print("Addition:", int(a) + int(b))
+    print("subtraction:", int(a) - int(b))
+    print("Multiplication:", int(a) * int(b))
+    print("Division:", int(a) / int(b))
 
 '''
 Task 2
@@ -18,4 +20,8 @@ Task 2
 
 first = input("Enter the first name : ")
 last = input("Enter the last name : ")
-print("Hello,",first,last+"!"," Welcome to the Python program.")
+
+if a.strip() == '' or b.strip() == '':
+    print("Error: One or both inputs are empty!")
+else:
+    print("Hello,",first,last+"!"," Welcome to the Python program.")
