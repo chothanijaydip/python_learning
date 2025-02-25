ddd9cbd8165a6446b4f551ccf2f459075e4e1d2b
Jaydip Chothani <32288904+chothanijaydip@users.noreply.github.com>
2025-02-25 12:29:18 +0530

    Update assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -2,13 +2,14 @@
 '''
 Task 1
 '''
-
+def is_number(value):
+    return value.strip().isdigit()
 a = input("Enter the first Number :")
 b = input("Enter the second Number :")
 
 if a.strip() == '' or b.strip() == '':
     print("Error: One or both inputs are empty!")
-else:
+if is_number(a) and is_number(b):
     print("Addition:", int(a) + int(b))
     print("subtraction:", int(a) - int(b))
     print("Multiplication:", int(a) * int(b))
