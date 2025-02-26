dbc6362fc40e42eb30a602f2c20cffef84608061
Jaydip Chothani <32288904+chothanijaydip@users.noreply.github.com>
2025-02-26 11:50:43 +0530

    Update assignment_4.py

--- assignment_4/assignment_4.py
+++ assignment_4/assignment_4.py
@@ -11,11 +11,12 @@ file1.close()
 try:
     with open("sample.txt",'r') as file:
         print("Reading file content:")
-        count = 1;
+        count = 1
         for line in file:
 
             print(f"Line {count}:"+line.strip())
             count = 1+count
+        file.close()
 except FileNotFoundError:
     print("Error: The file 'sample.txt' was not found.")
 
@@ -23,3 +24,24 @@ except FileNotFoundError:
 '''
 Task 2
 '''
+
+try:
+    fileContent1 = input("Enter a file content:")
+    file1 = open("output.txt",'w')
+    writeFile =file1.write(fileContent1)
+    if writeFile:
+        print("Data successfully written to output.txt.")
+    file1.close()
+    fileContent2 = input("Enter additional text to append:")
+    file1 = open("output.txt",'a')
+    appendFile =file1.write(fileContent2)
+    if appendFile:
+        print("Data successfully appended.")
+    file1.close()
+    file1 = open("output.txt",'r')
+    readFile =file1.read()
+    print('Final contents of output.txt')
+    print(readFile)
+    file1.close()
+except FileNotFoundError:
+    print("Error: The file 'output.txt' was not found.")
